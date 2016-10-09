0e555988c1e1bc1bc0884949f9f14bc475177c98
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2016-10-09 10:56:43 +0000

    Include grams in db

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,22 @@
 class GramsController < ApplicationController
   def index
   end
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = Gram.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  helper_method :gram_params
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  #devise_for :users
   root "grams#index"
+  resources :grams, only: [:new, :create]
 end
